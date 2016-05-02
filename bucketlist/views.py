4e9e69a393549ff87159c7303bea1df24421c8e9
Alex Kiura <alex.kiura@andela.com>
2016-05-02 23:37:23 +0300

    [Bug] Fix incorrect filter

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -7,7 +7,7 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date-joined')
+    queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 
