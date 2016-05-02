96f53a5e3aa85c36e503371bc3770256e676078c
Alex Kiura <alex.kiura@andela.com>
2016-05-02 23:35:12 +0300

    [Feature] Add API urls

--- yolo/urls.py
+++ yolo/urls.py
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from bucketlist import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
