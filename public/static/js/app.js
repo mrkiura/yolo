35f12fc2b5c5ff2c0c268641d048990d42abd2aa
Alex Kiura <alex.kiura@andela.com>
2016-05-03 09:58:53 +0300

    [Feature] Add appBar

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,5 +1,15 @@
-import React, { Component } from 'react';
 import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import React, { Component } from 'react';
+import AppBar from 'material-ui/lib/app-bar';
+import IconButton from 'material-ui/lib/icon-button';
+import NavigationClose from 'material-ui/lib/svg-icons/navigation/close';
+import IconMenu from 'material-ui/lib/menus/icon-menu';
+import MoreVertIcon from 'material-ui/lib/svg-icons/navigation/more-vert';
+import MenuItem from 'material-ui/lib/menus/menu-item';
+
+injectTapEventPlugin();
+
 
 class BucketListBox extends Component {
     render() {
@@ -17,6 +27,28 @@ class BucketListBox extends Component {
     }
 }
 
+
+const AppBarExampleIconMenu = () => (
+  <AppBar
+    title="Yolo"
+    //iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+    iconElementRight={
+      <IconMenu
+        iconButtonElement={
+          <IconButton><MoreVertIcon /></IconButton>
+        }
+        targetOrigin={{horizontal: 'right', vertical: 'top'}}
+        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+      >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+    }
+  />
+);
+
+export default AppBarExampleIconMenu;
 render(
-    <BucketListBox />, document.getElementById('yolo')
+    <AppBarExampleIconMenu />, document.getElementById('yolo')
 );
