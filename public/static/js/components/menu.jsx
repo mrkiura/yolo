decc6504e654de826a2f0b27ef61728fc7b2eead
Alex Kiura <alex.kiura@andela.com>
2016-05-07 00:53:19 +0300

    [Chore] Add menu bar

--- public/static/js/components/menu.jsx
+++ public/static/js/components/menu.jsx
@@ -10,25 +10,29 @@ import MenuItem from 'material-ui/lib/menus/menu-item';
 
 injectTapEventPlugin();
 
-const AppBarExampleIconMenu = () => (
-  <AppBar
-    title="Yolo"
-    //iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-    iconElementRight={
-      <IconMenu
-        iconButtonElement={
-          <IconButton><MoreVertIcon /></IconButton>
-        }
-        targetOrigin={{horizontal: 'right', vertical: 'top'}}
-        anchorOrigin={{horizontal: 'right', vertical: 'top'}}
-      >
-        <MenuItem primaryText="Refresh" />
-        <MenuItem primaryText="Help" />
-        <MenuItem primaryText="Sign out" />
-      </IconMenu>
+class Menu extends Component {
+    render() {
+        return(
+            <AppBar
+              title="Yolo"
+              iconElementRight={
+                <IconMenu
+                  iconButtonElement={
+                    <IconButton><MoreVertIcon /></IconButton>
+                  }
+                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                  anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+                >
+                  <MenuItem primaryText="Refresh" />
+                  <MenuItem primaryText="Help" />
+                  <MenuItem primaryText="Sign out" />
+                </IconMenu>
+              }
+            />
+        );
     }
-  />
-);
+
+}
 
 let routes = (<Router history={browserHistory}>
                 <Route path="/" component={Main}>
@@ -42,3 +46,5 @@ export default AppBarExampleIconMenu;
 render(
     <AppBarExampleIconMenu />, document.getElementById('yolo')
 );
+
+modue.exports = AppBar;
