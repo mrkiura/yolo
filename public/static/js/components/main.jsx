dcc2f12ecd28c2c7339efb654a0d462fcbad299f
Alex Kiura <alex.kiura@andela.com>
2016-05-09 01:00:11 +0300

    [Feature] Add children to main component

--- public/static/js/components/main.jsx
+++ public/static/js/components/main.jsx
@@ -1,1 +1,19 @@
-// The login page.
+import { render } from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import React, { Component } from 'react';
+
+class Main extends Component {
+   constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+module.exports = Main;
