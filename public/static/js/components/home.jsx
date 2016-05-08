76374d38dfae215ee9b9039d53a34fcfb0ebdc3b
Alex Kiura <alex.kiura@andela.com>
2016-05-09 01:01:01 +0300

    [Feature] Add index component

---
+++ public/static/js/components/home.jsx
@@ -0,0 +1,62 @@
+// The login page.
+import { render } from 'react-dom'
+import React, {Component} from 'react';
+import TextField from 'material-ui/lib/text-field';
+import request from 'superagent'
+import Tabs from 'material-ui/lib/tabs/tabs';
+import Tab from 'material-ui/lib/tabs/tab';
+import SwipeableViews from 'react-swipeable-views';
+import LoginForm from './loginForm.jsx';
+
+
+const styles = {
+    slide: {
+        padding: 10,
+    },
+};
+
+class AuthTabs extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            slideIndex: 0,
+        };
+    };
+
+    handleChange(value) {
+        this.setState({slideIndex: value});
+    }
+    render() {
+        return (
+            <div>
+                <Tabs
+                    onChange={this.handleChange.bind(this)}
+                    value={this.state.slideIndex}>
+                    <Tab label="login" value={0}></Tab>
+                    <Tab label="register" value={1}></Tab>
+                </Tabs>
+                <SwipeableViews
+                    index={this.state.slideIndex}
+                    onChangeIndex={this.handleChange}
+                >
+                    <div style={styles.slide}><LoginForm /></div>
+                    <div style={styles.slide}>Sign up tab</div>
+                </SwipeableViews>
+            </div>
+        );
+    }
+}
+
+class Home extends Component {
+    constructor() {
+        super();
+    }
+
+    render() {
+        return(
+            <div><AuthTabs/></div>
+        );
+    }
+}
+
+module.exports = Home;
