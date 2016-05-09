111dfcce6250d34428aa304ed58a989885312728
Alex Kiura <alex.kiura@andela.com>
2016-05-10 01:00:43 +0300

    [Feature] Add login button

--- public/static/js/components/loginForm.jsx
+++ public/static/js/components/loginForm.jsx
@@ -1,8 +1,47 @@
 import { render } from 'react-dom'
 import React, {Component} from 'react';
 import TextField from 'material-ui/lib/text-field';
+import RaisedButton from 'material-ui/lib/raised-button';
+
+const style = {
+  margin: 12,
+};
 
 export default class LoginForm extends Component {
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleFieldChange = this.handleFieldChange.bind(this);
+        this.state = {
+            username: '',
+            password: '',
+            token: ''
+        }
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        this.loginUser(this.state.username, this.state.password);
+    }
+    handleFieldChange(event) {
+        event.preventDefault();
+        let key = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            [key]: value
+        });
+    }
+    loginUser(username, password) {
+    request
+        .post('/api/v1/auth/login/')
+        .send({'username': username, 'password': password })
+        .end((err, result) => {
+            this.setState({
+                token: result.body.token
+            });
+            console.log(`response code is ${result.status}`);
+            console.log(this.state);
+        })
+}
     render() {
         return(
             <div>
@@ -18,6 +57,7 @@ export default class LoginForm extends Component {
                     type="password"
                     />
                 <br/>
+                <RaisedButton label="LOGIN" secondary={true} />
             </div>
         );
     }
