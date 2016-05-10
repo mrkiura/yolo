eca7c354219b48ba31ee42c45045476a7614e83a
Alex Kiura <alex.kiura@andela.com>
2016-05-10 03:14:47 +0300

    [Chore] Add docstrings to models

--- bucketlist/models.py
+++ bucketlist/models.py
@@ -2,14 +2,36 @@ from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
 class Bucketlist(models.Model):
+    """
+    Define a bucketlist.
+
+    Attributes:
+        user (ForeignKey): ForeignKey to the user.
+        list_name (str): A unique identifier for a bucketlist.
+        date_created (dateTime): Date of bucketlist creation.
+        date_modified (dateTime): Date of bucketlist modification.
+    """
+
     date_created = models.DateTimeField(auto_now_add=True)
     date_created = models.DateTimeField(auto_now=True)
     list_name = models.CharField(max_length=100, blank=True, default='')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, default=0)
 
+
 class BucketlistItem(models.Model):
+    """Define items in a user's bucketlist.
+
+    Attributes:
+        bucketlist (ForeignKey): ForeignKey to the bucketlist.
+        item_name (str): A unique identifier for a bucketlist.
+        priority (choice): An item's priority. High, Medium or Low.
+        date_created (dateTime): Date of bucketlist item creation.
+        done (bool): A flag to mark completed bucketlist items.
+        date_modified (dateTime): Date of bucketlist item modification.
+    """
+
     PRIORITIES = (
         ('H', 'High'),
         ('M', 'Medium'),
