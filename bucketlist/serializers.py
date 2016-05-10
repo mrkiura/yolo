4f6371d92786986651da0a7f5f9782229ad9cf2c
Alex Kiura <alex.kiura@andela.com>
2016-05-10 03:57:37 +0300

    [Chore] Add docstrings to models

--- bucketlist/serializers.py
+++ bucketlist/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-
+from models import Bucketlist, BucketlistItem
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -11,3 +11,13 @@ class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Group
         fields = ('url', 'name')
+
+class BucketlistSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bucketlist
+        fields = ('list_name', 'created_by', 'date_created', 'date_modified')
+
+class BucketlistItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BucketlistItem
+        fields = ('item_name', 'created_by', 'date_created', 'date_modified')
