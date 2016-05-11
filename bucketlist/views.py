cd14ffde5c79f4a095b2747eb29b747ce2c1a931
Alex Kiura <alex.kiura@andela.com>
2016-05-11 04:54:10 +0300

    [Feature] Add user creation

--- bucketlist/views.py
+++ bucketlist/views.py
@@ -1,19 +1,17 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from serializers import UserSerializer, GroupSerializer
+from serializers import UserSerializer, BucketlistSerializer, BucketlistItemSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserCreateViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
-
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+    def perform_create(self, serializer):
+        token = Token.objects.create(user=serializer.save())
+        # import ipdb; ipdb.set_trace()
