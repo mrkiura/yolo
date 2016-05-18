1df5ae0baf102a4169d25251e510b1681d3d6bb8
Alex Kiura <alex.kiura@andela.com>
2016-05-18 10:13:45 +0300

    [Chore] Restructure api

--- static/js/components/home.jsx
+++ static/js/components/home.jsx
@@ -1,16 +1,50 @@
 import { render } from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import React, { Component } from 'react';
+import TextField from 'material-ui/lib/text-field';
+import Paper from 'material-ui/lib/paper';
+import ActionNoteAdd from 'material-ui/lib/svg-icons/action/note-add';
+import IconButton from 'material-ui/lib/icon-button';
+
+
+const style = {
+    float: 'right',
+};
 
 class Home extends Component {
    constructor() {
         super();
+        this.state = {showAddButton: false};
+    }
+
+    onEnter() {
+        this.setState(
+            {showAddButton: !this.state.showAddButton}
+        );
     }
 
     render() {
         return (
             <div>
-                This is the home page.
+                <div className="list-input">
+                    <Paper zDepth={2}>
+                     <TextField
+                         className="text-input"
+                         underlineShow={false}
+                         hintText=" Add a bucketlist "
+                         onFocus={this.onEnter.bind(this)}
+                         onBlur={this.onEnter.bind(this)}
+                         />
+                     {this.state.showAddButton ?
+                         <IconButton
+                             style={style}
+                             tooltip="add bucketlsit">
+                             <ActionNoteAdd />
+                         </IconButton>
+                         : null
+                     }
+                 </Paper>
+                </div>
             </div>
         );
     }
