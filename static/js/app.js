f72e6df2e8fff27ef32b93204a20a2f8c00ca3ba
Alex Kiura <alex.kiura@andela.com>
2016-05-24 23:17:33 +0300

    [Feature] Add Login component to app routes

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,9 @@
 import { render } from 'react-dom';
 import Main from './components/main.jsx';
 import Home from './components/home.jsx';
-import Auth from './components/auth.jsx';
 import Landing from './components/landing.jsx';
+import SignupForm from './components/signupForm.jsx';
+import LoginForm from './components/loginForm.jsx';
 import React, { Component } from 'react';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
@@ -10,10 +11,11 @@ injectTapEventPlugin();
 
 let routes = (<Router history={browserHistory}>
                 <Route path="/" component={Main}>
-                    <IndexRoute component={Auth}/>
+                    <IndexRoute component={LoginForm}/>
                     <Route path="/home" component={Home}/>
-                    <Route path="/login" component={Auth}/>
+                    <Route path="/login" component={LoginForm}/>
                     <Route path="/bucketlists" component={Home}/>
+                    <Route path="/register" component={SignupForm}/>
                 </Route>
               </Router>)
 
