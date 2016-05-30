c43c48a02e3d23dc3cabddf00f978877d2dfdf86
Alex Kiura <alex.kiura@andela.com>
2016-05-30 19:51:23 +0300

    [Chore] Add drf docs

--- yolo/settings/base.py
+++ yolo/settings/base.py
@@ -41,6 +41,7 @@ PREREQ_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_nose',
+    'rest_framework_docs',
 ]
 
 PROJECT_APPS = [
@@ -76,7 +77,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
