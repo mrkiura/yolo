31eae21e655327ea3ea6ea602a57368e4b11e3de
Alex Kiura <alex.kiura@andela.com>
2016-05-30 04:42:34 +0300

    [Chore] Refactor URLs

--- yolo/urls.py
+++ yolo/urls.py
@@ -20,6 +20,7 @@ from api_v1.api import BucketListViewSet, BucketListItemViewSet,\
 from rest_framework_nested import routers
 from rest_framework_jwt.views import obtain_jwt_token
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 router = routers.SimpleRouter()
 router.register(r'bucketlists', BucketListViewSet)
@@ -31,10 +32,19 @@ bucketlist_router.register(r'items', BucketListItemViewSet,
                            base_name='bucketlists-items')
 
 urlpatterns = [
-    url(r'^api/v1/', include(router.urls)),
-    url(r'^api/v1/', include(bucketlist_router.urls)),
+    url(r'^api/v1/bucketlists/$',
+        BucketListViewSet.as_view({'get': 'list_bucketlists',
+                                   'post': 'create'})),
+    url(r'^api/v1/bucketlists/(?P<pk>[0-9]+)/$',
+        BucketListViewSet.as_view({'get': 'list_bucketlist',
+                                   'post': 'create'})),
+    url(r'^api/v1/bucketlists/(?P<pk_bucketlist>[0-9]+)/items/$',
+        BucketListItemViewSet.as_view({'post': 'create'})),
+    url(r'^bucketlists/(?P<pk_bucketlist>[0-9]+)/items/(?P<pk_item>[0-9]+)$',
+        BucketListItemViewSet.as_view({'put': 'update', 'delete': 'destroy'})),
     url(r'^api/v1/auth/login/', obtain_jwt_token),
-    url(r'^api/v1/auth/register/', UserCreateViewSet.as_view({'post': 'create'})),
+    url(r'^api/v1/auth/register/',
+        UserCreateViewSet.as_view({'post': 'create'})),
     url(r'^api/v1/', include('rest_framework.urls')),
     url(r'^', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', TemplateView.as_view(template_name='index.html')),
