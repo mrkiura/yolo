83a2dc650323bdfe06b8eeee7be2b7018f4d2617
Alex Kiura <alex.kiura@andela.com>
2016-06-04 03:17:01 +0300

    [Chore] Update settings

--- yolo/settings/test.py
+++ yolo/settings/test.py
@@ -118,22 +118,6 @@ STATIC_ROOT = 'staticfiles'
 
 WSGI_APPLICATION = 'yolo.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'yolo',
-        'USER': 'heavy_machinery',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
 
