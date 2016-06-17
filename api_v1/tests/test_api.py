3aa01e20a4fcd308e06bd16d34474f8049dfdd19
Alex Kiura <alex.kiura@andela.com>
2016-06-17 12:41:02 +0300

    [Fix] Fix failing tests

--- api_v1/tests/test_api.py
+++ api_v1/tests/test_api.py
@@ -7,10 +7,15 @@ from api_v1.models import User
 class UserActions(APITestCase):
     def setUp(self):
         """add dummy user."""
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'arafat',
+                          'password': 'nomasana',
+                          'confirm_password': 'nomasana',
+                          'email': 'arafat@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'arafat',
-                                     'password': 'nomasana',
-                                     'email': 'arafat@yolo.com'},
+                                     'password': 'nomasana'},
                                     format='json')
         self.token = response.data['token']
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + self.token)
@@ -21,6 +26,7 @@ class UserActions(APITestCase):
         response = self.client.post('/api/v1/auth/register/',
                                     {'username': 'rustic',
                                      'password': '123',
+                                     'confirm_password': '123',
                                      'email': 'rustic@gmail.com'},
                                     format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -41,10 +47,15 @@ class UserActions(APITestCase):
 
 class BucketlistActions(APITestCase):
     def setUp(self):
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'arafat',
+                          'password': 'nomasana',
+                          'confirm_password': 'nomasana',
+                          'email': 'makmende@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'arafat',
-                                     'password': 'nomasana',
-                                     'email': 'makmende@yolo.com'},
+                                     'password': 'nomasana'},
                                     format='json')
         self.token = response.data['token']
         self.client.force_authenticate(token=self.token)
@@ -72,13 +83,18 @@ class BucketlistActions(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_unauthorised_bucketlist_access(self):
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'catalyst',
+                          'password': 'motosana',
+                          'confirm_password': 'motosana',
+                          'email': 'catalyst@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'catalyst',
-                                     'password': 'motosana',
-                                     'email': 'catalyst@yolo.com'},
+                                     'password': 'motosana'},
                                     format='json')
-        gudaman_token = response.data['token']
-        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + gudaman_token)
+        catalyst_token = response.data['token']
+        self.client.credentials(HTTP_AUTHORIZATION='JWT ' + catalyst_token)
         response2 = self.client.get('/api/v1/bucketlists/' +
                                     self.first_bucketlist_id + '/')
         self.assertEqual(response2.status_code, status.HTTP_401_UNAUTHORIZED)
@@ -98,10 +114,15 @@ class BucketlistActions(APITestCase):
         self.assertContains(response, 'the world')
 
     def test_unauthorized_bucketlist_update(self):
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'shakur',
+                          'password': 'motosana',
+                          'confirm_password': 'motosana',
+                          'email': 'shakur@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'shakur',
-                                     'password': 'motosana',
-                                     'email': 'shakur@yolo.com'},
+                                     'password': 'motosana'},
                                     format='json')
         shakur_token = response.data['token']
         self.client.credentials(HTTP_AUTHORIZATION='JWT ' + shakur_token)
@@ -122,10 +143,15 @@ class BucketlistActions(APITestCase):
 
 class BucketlistItemActions(APITestCase):
     def setUp(self):
-        auth_response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'bond007',
+                          'password': 'nomasana',
+                          'confirm_password': 'nomasana',
+                          'email': 'bond007@yolo.com'},
+                         format='json')
+        auth_response = self.client.post('/api/v1/auth/login/',
                                          {'username': 'bond007',
-                                          'password': 'nomasana',
-                                          'email': 'bond007@yolo.com'},
+                                          'password': 'nomasana'},
                                          format='json')
         self.token = auth_response.data['token']
         self.client.force_authenticate(token=self.token)
@@ -162,9 +188,16 @@ class BucketlistItemActions(APITestCase):
         self.assertContains(put_response, 'China')
 
     def test_unauthorized_item_update(self):
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'alpacino',
+                          'password': 'motosana',
+                          'confirm_password': 'motosana',
+                          'email': 'alpacino@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'alpacino',
                                      'password': 'motosana',
+                                     'confirm_password': 'motosana',
                                      'email': 'alpacino@yolo.com'},
                                     format='json')
         post_response = self.client.post('/api/v1/bucketlists/' +
@@ -180,11 +213,16 @@ class BucketlistItemActions(APITestCase):
                                     format='json')
         self.assertEqual(response2.status_code, status.HTTP_401_UNAUTHORIZED)
     def test_unauthorised_item_delete(self):
-        response = self.client.post('/api/v1/auth/register/',
+        self.client.post('/api/v1/auth/register/',
+                         {'username': 'fergie',
+                          'password': 'motosana',
+                          'confirm_password': 'motosana',
+                          'email': 'fergie@yolo.com'},
+                         format='json')
+        response = self.client.post('/api/v1/auth/login/',
                                     {'username': 'fergie',
                                      'password': 'motosana',
-                                     'email': 'fergie@yolo.com'},
-                                    format='json')
+                                     })
         post_response = self.client.post('/api/v1/bucketlists/' +
                                          self.first_bucketlist_id + '/items/',
                                          {'item_name': 'Diani'},
